117cf73cb6e082bb7333306be797ebe77242875b
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-08 00:11:26 +0100

    Updating the tests

---
+++ tests/Stubs/NotFilterable.php
@@ -0,0 +1,9 @@
+<?php namespace Arcanedev\Sanitizer\Tests\Stubs;
+
+/**
+ * Class     NotFilterable
+ *
+ * @package  Arcanedev\Sanitizer\Tests\Stubs
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class NotFilterable {}
