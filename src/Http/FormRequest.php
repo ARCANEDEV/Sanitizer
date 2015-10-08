54dc0dd0777b305737e08747ab48dac4c1921832
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-08 03:04:55 +0100

    Adding FormRequest

---
+++ src/Http/FormRequest.php
@@ -0,0 +1,49 @@
+<?php namespace Arcanedev\Sanitizer\Http;
+
+use Arcanedev\Support\Bases\FormRequest as BaseFormRequest;
+
+/**
+ * Class     FormRequest
+ *
+ * @package  Arcanedev\Sanitizer\Http
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+abstract class FormRequest extends BaseFormRequest
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Sanitize input before validating.
+     */
+    public function validate()
+    {
+        $this->sanitize();
+
+        parent::validate();
+    }
+
+    /**
+     * Sanitize this request's input.
+     */
+    protected function sanitize()
+    {
+        $sanitized = sanitizer()->make(
+            $this->input(),
+            $this->sanitizerRules()
+        );
+
+        $this->replace($sanitized);
+    }
+
+    /**
+     * Sanitizer's rules to be applied to the input.
+     *
+     * @return array
+     */
+    protected function sanitizerRules()
+    {
+        return [];
+    }
+}
